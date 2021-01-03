e45be2c9a24d2224751560d4ba170559d7e0ed8a
marina0715 <74341599+marina0715@users.noreply.github.com>
2021-01-04 03:03:00 +0900

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -7,3 +10,7 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.IMAGE_URL, document_root=settings.IMAGE_ROOT)
